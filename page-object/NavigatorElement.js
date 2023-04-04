bd4edbeef723d8ba572f95bd22c71bff725350c4
msulaev <ewrewr>
2023-04-04 14:29:48 +0300

    Add goToCheckout method to NavigationElement

--- page-object/NavigatorElement.js
+++ page-object/NavigatorElement.js
@@ -2,11 +2,17 @@ export class NavigationElement {
     constructor(page){
         this.page = page;
         this.basketCounter = page.locator('[data-qa=header-basket-count]');
+        this.checkoutLink = page.locator('[data-qa=desktop-nav-link]').filter({ hasText: 'Checkout' });
     }
 
     async getBasketCounter() {
         const text = await this.basketCounter.innerText();
         return parseInt(text,10);
+    }
+
+    async goToCheckout() {
+        await this.checkoutLink.click();
+        await this.page.waitForURL("http://localhost:2221/basket");
     }
 
 }
--- tests/new_user_full_journey.spec.js
+++ tests/new_user_full_journey.spec.js
@@ -1,12 +1,15 @@
 import { test } from "@playwright/test"
 import { ProductsPage } from "../page-object/ProductPage"
+import { NavigationElement } from "../page-object/NavigatorElement"
 
 test.only("New user full e2e test journey", async ({ page }) =>{
     const productPage = new ProductsPage(page);
+    const navigationElement = new NavigationElement(page);
     await productPage.visit();
     await productPage.addProductToBusket(0);
     await productPage.addProductToBusket(1);
     await productPage.addProductToBusket(2);
-    await page.pause();
+    await navigationElement.goToCheckout();
+
 })
 
