8b9aa2c1a1932af2d148bd8216288cf75918177c
msulaev <ewrewr>
2023-04-05 12:33:20 +0300

    Add new page and few methods

--- page-object/ProductPage.js
+++ page-object/ProductPage.js
@@ -5,6 +5,8 @@ export class ProductsPage {
     constructor(page) {
         this.page = page;
         this.addBtn = page.locator('[data-qa=product-button]');
+        this.dropdown = page.locator('[data-qa=sort-dropdown]');
+        this.productTitle = page.locator('[data-qa=product-title]');
     }
 
     async visit() {
@@ -13,7 +15,7 @@ export class ProductsPage {
 
     async addProductToBusket(index) {
        const btn = this.addBtn.nth(index);
-       const navigationElement = new NavigationElement(this.page);
+       const navigationElement = new NavigationElement(this.page)
        await btn.waitFor();
        await expect(btn).toHaveText("Add to Basket");
        const basketCountBefore = await navigationElement.getBasketCounter();
@@ -21,5 +23,16 @@ export class ProductsPage {
        await expect(btn).toHaveText("Remove from Basket");
        const basketCountAfter = await navigationElement.getBasketCounter();
        expect(basketCountAfter).toBeGreaterThan(basketCountBefore);
+    }
+
+    async sortByCheapest() {
+        await this.dropdown.waitFor();
+        await this.productTitle.first().waitFor();
+        const productTitlesBefore = await this.productTitle.allInnerTexts();
+        console.warn(productTitlesBefore);
+        await this.dropdown.selectOption('price-asc');
+        const productTitlesAfter = await this.productTitle.allInnerTexts();
+        console.warn(productTitlesAfter);
+        expect(productTitlesAfter).not.toEqual(productTitlesBefore);
     }
 }
--- tests/new_user_full_journey.spec.js
+++ tests/new_user_full_journey.spec.js
@@ -1,15 +1,25 @@
 import { test } from "@playwright/test"
 import { ProductsPage } from "../page-object/ProductPage"
 import { NavigationElement } from "../page-object/NavigatorElement"
+import { CheckoutPage } from "../page-object/CheckoutPage"
+import { LoginPage } from "../page-object/LoginPage"
 
 test.only("New user full e2e test journey", async ({ page }) =>{
     const productPage = new ProductsPage(page);
     const navigationElement = new NavigationElement(page);
     await productPage.visit();
+    await productPage.sortByCheapest();
     await productPage.addProductToBusket(0);
     await productPage.addProductToBusket(1);
     await productPage.addProductToBusket(2);
     await navigationElement.goToCheckout();
+
+    const checkoutPage = new CheckoutPage(page);
+    await checkoutPage.removeCheapestItem();
+    await checkoutPage.continueToCheckout();
+
+    const loginPage = new LoginPage(page);
+    await loginPage.login();
 
 })
 
