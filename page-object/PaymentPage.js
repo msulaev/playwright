3db643b8691fd05c257e7aefc608b894655508b4
Mikhail Sulaev <49402569+msulaev@users.noreply.github.com>
2023-11-13 22:24:52 +0200

    Complete activateDiscount

--- page-object/PaymentPage.js
+++ page-object/PaymentPage.js
@@ -5,19 +5,26 @@ export class PaymentPage { // work with iframe
                                 .locator('[data-test="discount-code]');
         this.discountInput = page.getByPlaceholder('Discount code'); 
         this.activateDiscountBtn = page.locator('[data-qa="submit-discount-button]');
-
+        this.totalValue = page.locator('[data-qa="total-value"]');
+        this.discountedValue = page.locator('[data-qa="total-with-discount-value"]');
+        this.discountActiveMessage = page.locator('[data-qa="discount-active-message"]');      
     }
 
     async activateDiscount() { 
-        await this.page.discountCode.waitFor();
-        const code = await this.discountCode.innerText();
-        await this.discountInput.waitFor();
-     //   await this.discountInput.fill(code);
-     //   await expect(this.discountInput).toHaveValue(code);
-        await this.discountInput.focus();
-        await this.discountInput.type(code, {delay: 1000});
-        expect(await this.discountInput.inputValue()).toBe(code);
-        await this.activateDiscountBtn.waitFor();
-        await this.activateDiscountBtn.click();
+        expect(await this.discountedValue.isVisible()).toBe(false)
+        expect(await this.discountActiveMessage.isVisible()).toBe(false)
+        await this.activateDiscountButton.waitFor()
+        await this.activateDiscountButton.click()
+        await this.discountActiveMessage.waitFor()
+        await this.discountedValue.waitFor()
+        const discountValueText = await this.discountedValue.innerText() // "345$"
+        const discountValueOnlyStringNumber = discountValueText.replace("$", "")
+        const discountValueNumber = parseInt(discountValueOnlyStringNumber, 10)
+
+        await this.totalValue.waitFor()
+        const totalValueText = await this.totalValue.innerText() // "345$"
+        const totalValueOnlyStringNumber = totalValueText.replace("$", "")
+        const totalValueNumber = parseInt(totalValueOnlyStringNumber, 10)
+        expect(discountValueNumber).toBeLessThan(totalValueNumber)
     }
 }
